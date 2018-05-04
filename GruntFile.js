db16a191f29544dd864dee4efc29fe792c37a5cb
sdivelbiss <scottdivelbiss@gmail.com>
2018-05-04 15:11:28 -0700

    adding grunt-contrib-concat dependency

---
+++ GruntFile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt){
+    // Config
+    grunt.initConfig({
+        //pass in options to plugins, references to files, etc.
+
+    });
+
+    // Load plugins
+    //grunt.loadNpmTasks('')
+
+    //Register tasks
+
+}
